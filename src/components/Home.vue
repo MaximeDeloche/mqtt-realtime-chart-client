e320c466f339984e0823545f0b150f4756687ce9
MaximeDeloche <58418423+MaximeDeloche@users.noreply.github.com>
2021-07-05 14:12:39 +0200

    Update Home component and change default config to listen from all sources on port 9080

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,6 @@
     <div>
         <div class="container-fluid">
             <div class="row text-center">
-                <div class="col-xs-12 col-lg-4 col-lg-offset-4">
-                    <img src="../assets/logo.png" width="100" height="100">
-                </div>
                 
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
                     <h1>Status: {{connStatus}}</h1>
@@ -12,7 +9,7 @@
                     <!-- Panel div start -->
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Magnitude</h3>
+                            <h3 class="panel-title">Title</h3>
                         </div>
                         <div class="panel-body">
                             <!-- Chart container -->
@@ -23,30 +20,12 @@
                             <!-- End of chart container -->
                         </div>
                         <div class="panel-footer">
-                            <p v-if="displayedValues.length > 0">
-                                <small>
-                                    <span v-bind:style="{ color: dvColors.v1}">{{displayedValues[0].v1}} </span>V
-                                    | <span v-bind:style="{ color: dvColors.v2}">{{displayedValues[0].v2}} </span>V
-                                    | <span v-bind:style="{ color: dvColors.v3}">{{displayedValues[0].v3}} </span>V
-                                </small>
-                            </p>
+                            <h3 class="panel-title">Footer</h3>
                         </div>
                     </div>
                     <!-- Panel div end -->
-                    
-                    <!-- Range slider chart-refresh control -->
-                    <div class="col-xs-6 col-xs-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2">
-                        <input v-model="renderEveryNth" type="range" min="1" max="9" value="5">
-                        <p>Render after <strong>{{renderEveryNth}}</strong> message(s)</p>
-                    </div>
-                    <!-- End of range slider -->
                 </div>
             </div>
-            <!-- Footer -->
-            <div class="footy text-center">
-                <p>Created with <span class="glyphicon glyphicon-heart-empty"></span> by Nick Jokic.</p>
-            </div>
-            <!-- End of footer -->
         </div>
     </div>
 </template>
@@ -56,57 +35,48 @@
     import Rickshaw from 'rickshaw'
     import 'rickshaw/rickshaw.min.css'
     import 'bootstrap/dist/css/bootstrap.css'
-    var socket = io.connect("http://localhost:3000");
+    var url = new URL(window.location);
+    url.port = parseInt(url.port) + 1;
+    var socket = io.connect(url.toString());
     var magnitudeChart;
 
     export default {
         name: 'home',
         data() {
             return {
-                messageSeries: [],
-                renderEveryNth: 5,
-                updateInterval: 20,
-                streamFrequency: 50,
+				currentIndex: 0,
+				maxLength: 100,
+                updateInterval: 200,
                 connStatus: "Disconnected",
-                messageIndex: 0,
-                displayedValues: [],
-                dvColors: {
-                    v1: "#cb503a",
-                    v2: "#72c039",
-                    v3: "#65b9ac"
-                }
             }
         },
         mounted() {
             this.initChart();
             this.openSocketListeners();
         },
-        watch: {
-            renderEveryNth: function() {
-                this.messageSeries = [];
-                this.messageIndex = 0;
-            }
-        },
         methods: {
             /* Rickshaw.js initialization */
             initChart() {
                 magnitudeChart = new Rickshaw.Graph({
                     element: document.querySelector("#demo_chart"),
-                    width: "500",
-                    height: "180",
+                    width: "1000", /* 500 */
+                    height: "500", /* 180 */
                     renderer: "line",
-                    min: 228,
-                    max: 248,
-                    series: new Rickshaw.Series.FixedDuration([{
-                        name: 'v1',
-                        color: '#EC644B'
-                    }], undefined, {
-                        timeInterval: this.updateInterval,
-                        maxDataPoints: 100,
-                        timeBase: new Date().getTime() / 1000
-
-                    })
-                });
+                    min: 50,
+                    max: 100,
+                    series: [
+						{
+							name: "reals",
+							data: [],
+							color: "#254661"
+						},
+						{
+							name: "predictions",
+							data: [],
+							color: "#00a65e"
+						}
+					]
+					});
 
                 var y_axis = new Rickshaw.Graph.Axis.Y({
                     graph: magnitudeChart,
@@ -124,37 +94,47 @@
                     this.resizeChart(magnitudeChart)
                 });
 
+				// first series contains real prices, the second one the predicted next steps
+				this.realPrices = magnitudeChart.series[0];
+				this.predPrices = magnitudeChart.series[1];
+
             },
+
+
             resizeChart(chart) {
                 chart.configure({
                     width: this.$refs.panel.clientWidth,
                 });
                 chart.render();
             },
+
+
             /* Insert received datapoints into the chart */
-            insertDatapoints(messages, chart) {
-                for (let i = 0; i < messages.length; i++) {
-                    let voltageData = {
-                        Magnitude1: messages[i].v1,
-                        Magnitude2: messages[i].v2,
-                        Magnitude3: messages[i].v3
-                    };
-                    chart.series.addData(voltageData);
-                }
+            insertDatapoints(message, chart) {
+				// message contains:
+				// {price: new real price from the last second,
+				//  predictions: [list of predicted next prices]}
+
+				// Step 1: update real prices
+				// add new real price (with current index) to the list
+				this.realPrices.data.push({x: this.currentIndex, y: message.price});
+				// pop oldest element(s) if list reached max length
+				while(this.realPrices.data.length > this.maxLength) {
+					this.realPrices.data.shift();
+				}
+				this.currentIndex ++; // increment current index for the next real price
+
+				// Step 2: replace predicted prices
+				var predIndex = this.currentIndex;
+				// map predictions to indexes that follow the current index ('future' indexes)
+				this.predPrices.data = message.predictions.map(elt => ({"x": predIndex++, "y": elt}));
+				// prepend the last real price for the junction of the 2 plots
+				this.predPrices.data.unshift(this.realPrices.data.slice(-1)[0]);
+
                 chart.render();
             },
-            /* Update displayed values every second on average */
-            updateDisplayedValues() {
-                if (this.messageIndex == this.streamFrequency) {
-                    this.messageIndex = 0;
-                    this.displayedValues = this.messageSeries;
-                } else if (this.messageIndex == 0) {
-                    this.displayedValues = this.messageSeries;
-                    this.messageIndex++;
-                } else {
-                    this.messageIndex++;
-                }
-            },
+
+
             openSocketListeners() {
                 socket.on('connect', () => {
                     this.connStatus = "Connected";
@@ -164,22 +144,9 @@
                     this.connStatus = "Disconnected";
                 });
 
-                /* Update chart after every #renderEveryNth message */
-                socket.on('voltageData', (message) => {
-
-                    /* Check if displayed values have to be updated */
-                    this.updateDisplayedValues();
-
-                    /* Push stream data to current series, if it's not yet render-time */
-                    if (this.messageSeries.length < this.renderEveryNth) {
-                        this.messageSeries.push(message);
-                    }
-
-                    /* Render-time! */
-                    if (this.messageSeries.length == this.renderEveryNth) {
-                        this.insertDatapoints(this.messageSeries, magnitudeChart);
-                        this.messageSeries = [];
-                    }
+                /* Update chart when price is received */
+                socket.on('priceData', (message) => {
+					this.insertDatapoints(message, magnitudeChart);
                 });
             }
         }
